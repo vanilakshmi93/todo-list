c2306e76428ebea19b39eda79c7836b17512909f
Vani lakshmi <lakshmi.bebrave@gmail.com>
2025-10-08 21:53:21 +0530

    lasts

--- client/src/contexts/ThemeContext.tsx
+++ client/src/contexts/ThemeContext.tsx
@@ -1,4 +1,4 @@
-import React, { createContext, useState, useContext, useEffect, ReactNode } from 'react';
+import React, { createContext, useState, useContext, useEffect,type ReactNode } from 'react';
 
 type ThemeType = 'light' | 'dark';
 
